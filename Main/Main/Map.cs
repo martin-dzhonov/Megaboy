e5faa3a260d559ac40ed6546df24df116f674a22
Martin Dzhonov <martin.dzhonov@gmail.com>
2014-02-19 23:25:17 +0200

    Added map and background

---
+++ Main/Main/Map.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+
+namespace Main
+{
+    public enum WindowSize
+    {
+        Width = 1100, Height = 550
+    }
+    class Map
+    {
+        private List<CollisionTile> collisionTiles = new List<CollisionTile>();
+
+        public List<CollisionTile> CollisionTiles
+        {
+            get { return this.collisionTiles; }
+        }
+
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        public void Generate(int[,] map, int size)
+        {
+            for (int i = 0; i < map.GetLength(1); i++)
+            {
+                for (int j = 0; j < map.GetLength(0); j++)
+                {
+                    int number = map[j, i];
+
+                    if (number > 0)
+                    {
+                        collisionTiles.Add(new CollisionTile(number, new Rectangle(i * size, j * size, size, size)));
+                    }
+
+                    this.Width = (i + 1) * size;
+                    this.Height = (j + 1) * size;
+                }
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            foreach (var tile in collisionTiles)
+            {
+                tile.Draw(spriteBatch);
+            }
+        }
+    }
+}
---
+++ Main/Main/Tiles.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Main
+{
+    class Tiles
+    {
+        protected Texture2D texture;
+        public Rectangle Rectangle { get; set; }
+        public static ContentManager Content { get; set; }
+
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(this.texture, this.Rectangle, Color.White);
+        }
+    }
+
+    class CollisionTile : Tiles
+    {
+        public CollisionTile(int i, Rectangle rectangle)
+        {
+            this.texture = Content.Load<Texture2D>("Tile" + i);
+            this.Rectangle = rectangle;
+        }
+    }
+}
