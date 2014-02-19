953854f031e66567365d1eeb228d9d5f7934f2bc
Martin Dzhonov <martin.dzhonov@gmail.com>
2014-02-19 23:27:43 +0200

    Added player

---
+++ Main/Main/Animation.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Main
+{
+    class Animation
+    {
+        Texture2D strip;
+        Vector2 position;
+        Rectangle sourceRectangle;
+        Rectangle drawRectangle;
+        public bool LookingRight { get; private set; }
+        int currentFrame;
+        int frameHeight;
+        int frameWidth;
+        float timer;
+        float interval = 60;
+
+        const int FRAMES_PER_ROW = 8;
+        const int NUM_ROWS = 1;
+        const int NUM_FRAMES = 8;
+
+        private const string STRIP_NAME = "16jpo1w";
+        private const int PLAYER_SIZE = 60;
+
+        public void Load(ContentManager contentManager)
+        {
+            strip = contentManager.Load<Texture2D>(STRIP_NAME);
+
+            // calculate frame size
+            frameWidth = strip.Width / FRAMES_PER_ROW;
+            frameHeight = strip.Height / NUM_ROWS;
+
+            // set initial draw and source rectangles
+            sourceRectangle = new Rectangle(0, 0, frameWidth, frameHeight);
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            drawRectangle = new Rectangle((int)position.X, (int)position.Y, PLAYER_SIZE, PLAYER_SIZE);
+            spriteBatch.Draw(strip, drawRectangle, sourceRectangle, Color.White);
+        }
+
+        public void Update(GameTime gameTime, int playerX, int playerY)
+        {
+            sourceRectangle = new Rectangle(currentFrame * frameWidth, 0, frameWidth, frameHeight);
+            position.X = playerX;
+            position.Y = playerY;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                this.LookingRight = true;
+                AnimateRight(gameTime);
+            }
+            else if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                this.LookingRight = false;
+                AnimateLeft(gameTime);
+            }
+        }
+
+        public void AnimateRight(GameTime gameTime)
+        {
+            timer += (float)gameTime.ElapsedGameTime.TotalMilliseconds / 2;
+            if (timer > interval)
+            {
+                currentFrame++;
+                timer = 0;
+            }
+            if (currentFrame >= 4)
+            {
+                currentFrame = 0;
+            }
+        }
+        public void AnimateLeft(GameTime gameTime)
+        {
+            timer += (float)gameTime.ElapsedGameTime.TotalMilliseconds / 2;
+            if (timer > interval)
+            {
+                currentFrame++;
+                timer = 0;
+            }
+            if (currentFrame > 7 || currentFrame < 4)
+            {
+                currentFrame = 4;
+            }
+        }
+    }
+}
---
+++ Main/Main/Player.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Main
+{
+    class Player
+    {
+        private Texture2D texture;
+        private Vector2 position = new Vector2(63, 63);
+        private Vector2 velocity;
+        private Rectangle rectangle;
+        private bool hasJumped = false;
+
+        private string spriteName = "knight1";
+        private const int PLAYER_SIZE = 60;
+
+        public Vector2 Position
+        {
+            get
+            {
+                return this.position;
+            }
+        }
+
+        public int X
+        {
+            get { return (int)this.position.X; }
+        }
+
+        public int Y
+        {
+            get { return (int)this.position.Y; }
+        }
+        public void Load(ContentManager Content)
+        {
+            texture = Content.Load<Texture2D>(spriteName);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            position += velocity;
+            rectangle = new Rectangle((int)position.X, (int)position.Y, PLAYER_SIZE, PLAYER_SIZE);
+
+            ReadInput(gameTime);
+
+            if (velocity.Y < 10)
+            {
+                velocity.Y += 0.4f;
+            }
+        }
+
+        private void ReadInput(GameTime gameTime)
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                velocity.X = (float)gameTime.ElapsedGameTime.TotalMilliseconds / 3;
+            }
+            else if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                velocity.X = -(float)gameTime.ElapsedGameTime.TotalMilliseconds / 3;
+            }
+            else
+            {
+                velocity.X = 0f;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Up) && hasJumped == false)
+            {
+                position.Y -= 5f;
+                velocity.Y = -9f;
+                hasJumped = true;
+            }
+
+
+        }
+        public void Collision(Rectangle newRectangle, int xOffset, int yOffset)
+        {
+            if (rectangle.TouchTopOf(newRectangle))
+            {
+                rectangle.Y = newRectangle.Y - rectangle.Height;
+                velocity.Y = 0f;
+                hasJumped = false;
+            }
+            if (rectangle.TouchLeftOff(newRectangle))
+            {
+                position.X = newRectangle.X - rectangle.Width - 2;
+            }
+            if (rectangle.TouchRightOff(newRectangle))
+            {
+                position.X = newRectangle.X + newRectangle.Width + 2;
+            }
+            if (rectangle.TouchBottomOf(newRectangle))
+            {
+                velocity.Y = 1f;
+            }
+            if (position.X < 0)
+            {
+                position.X = 0;
+            }
+            if (position.X > xOffset - rectangle.Width)
+            {
+                position.X = xOffset - rectangle.Width;
+            }
+            if (position.Y < 0)
+            {
+                velocity.Y = 1f;
+            }
+            if (position.Y > yOffset - rectangle.Height)
+            {
+                position.Y = yOffset - rectangle.Height;
+            }
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, rectangle, Color.White);
+        }
+
+    }
+}
