28b80a43b343bd3a75684d38dea78b81bfa19464
Martin Dzhonov <martin.dzhonov@gmail.com>
2014-02-20 22:44:24 +0200

    Fixed jumping v2

--- Main/Main/Player.cs
+++ Main/Main/Player.cs
@@ -84,7 +84,7 @@ namespace Main
 
             ReadInput(gameTime);
 
-            if (velocity.Y < 15)
+            if (velocity.Y < 12)
             {
                 velocity.Y += 0.4f;
             }
