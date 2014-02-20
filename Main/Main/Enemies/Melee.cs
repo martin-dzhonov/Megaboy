bc327f285981ed6e9bf49496888051bf4162659f
Martin Dzhonov <martin.dzhonov@gmail.com>
2014-02-20 21:21:55 +0200

    Added namespaces and stuff

---
+++ Main/Main/Enemies/Melee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Main
+{
+    class Melee : Enemy
+    {
+        public override void Update(GameTime gameTime)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Load(ContentManager contentManager)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
