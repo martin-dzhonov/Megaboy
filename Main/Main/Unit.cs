c73d127e663dd8064f11f1d8d7d4ac5ff9a4b302
YoTsenkov <yotsenkov@gmail.com>
2014-02-20 13:25:57 +0200

    abstract class Unit

---
+++ Main/Main/Unit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Main
+{
+    public abstract class Unit : IUnit
+    {
+        public abstract void Update(Microsoft.Xna.Framework.GameTime gameTime);
+
+        public abstract void Load(Microsoft.Xna.Framework.Content.ContentManager contentManager);
+
+        public abstract void Draw(Microsoft.Xna.Framework.Graphics.SpriteBatch spriteBatch);
+
+    }
+}
