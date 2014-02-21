789df2f7e37990d48b45d83432825851c260a889
YoTsenkov <yotsenkov@gmail.com>
2014-02-21 15:13:55 +0200

    Interface for creating maps

---
+++ Main/Main/Interfaces/IMap.cs
@@ -0,0 +1,17 @@
+﻿namespace Main.Interfaces
+{
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Audio;
+    using Microsoft.Xna.Framework.Content;
+    using Microsoft.Xna.Framework.GamerServices;
+    using Microsoft.Xna.Framework.Graphics;
+    using Microsoft.Xna.Framework.Input;
+    using Microsoft.Xna.Framework.Media;
+    public interface IMap
+    {
+        int Width { get; }
+        int Height { get; }
+        void Generate(int[,] map, int size);
+        void Draw(SpriteBatch spriteBatch);
+    }
+}
