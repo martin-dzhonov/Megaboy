6c29df64b34bc47b8cddc46ef3b488fbdb69b3f2
Martin Dzhonov <martin.dzhonov@gmail.com>
2014-02-23 18:41:04 +0200

    Fixed collision

--- Main/Main/RectangleHelper.cs
+++ Main/Main/RectangleHelper.cs
@@ -9,9 +9,9 @@ namespace Main
         public static bool TouchTopOf(this Rectangle r1, Rectangle r2)
         {
             return (r1.Bottom >= r2.Top - 1 &&
-                r1.Bottom <= r2.Top + (r2.Height / 2) &&
-                r1.Right >= r2.Left + r2.Width / 4 &&
-                r1.Left <= r2.Right - r2.Width / 4);
+                r1.Bottom <= r2.Top + (r2.Height / 5) &&
+                r1.Right >= r2.Left + r2.Width / 5 &&
+                r1.Left <= r2.Right - r2.Width / 5);
         }
 
         public static bool TouchBottomOf(this Rectangle r1, Rectangle r2)
@@ -26,16 +26,16 @@ namespace Main
         {
             return (r1.Right <= r2.Right &&
                 r1.Right >= r2.Left - 3 &&
-                r1.Top <= r2.Bottom - (r2.Width / 2) &&
-                r1.Bottom >= r2.Top + (r2.Width / 2));
+                r1.Top <= r2.Bottom - (r2.Width / 10) &&
+                r1.Bottom >= r2.Top + (r2.Width / 10));
         }
 
         public static bool TouchRightOff(this Rectangle r1, Rectangle r2)
         {
             return (r1.Left >= r2.Left &&
                 r1.Left <= r2.Right + 3 &&
-                r1.Top <= r2.Bottom - (r2.Width / 3) &&
-                r1.Bottom >= r2.Top + (r2.Width / 3));
+                r1.Top <= r2.Bottom - (r2.Width / 10) &&
+                r1.Bottom >= r2.Top + (r2.Width / 10));
         }
         public static bool ProjectileTouchTopOf(this Rectangle r1, Rectangle r2)
         {
