561fc39f2cbedfd73ec65b25e43c97d8754d07b7
lovechguy <lovechguy@gmail.com>
2014-02-24 21:04:03 +0200

    MaleNpc

---
+++ Main/Main/Units/NPCs/MaleNpc.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Main.Units.NPCs
+{
+    public class MaleNpc : NPC
+    {
+        public MaleNpc(int positionX, int positionY) : base(positionX, positionY)
+        {
+            this.spriteName = "maleNpc";
+        }
+
+        public override bool isReached(Player player)
+        {
+            if (((this.Position.X >= player.Position.X - 5) && (this.Position.X <= player.Position.X + 5)) &&
+                this.Position.Y == player.Position.Y)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override void Load(Microsoft.Xna.Framework.Content.ContentManager contentManager)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Draw(Microsoft.Xna.Framework.Graphics.SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
