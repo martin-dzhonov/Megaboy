97fd0121c0b8e1d25f25fc9b81b2723810147f77
YoTsenkov <yotsenkov@gmail.com>
2014-02-24 18:32:55 +0200

    interface from projectiles

---
+++ Main/Main/Interfaces/IProjectile.cs
@@ -0,0 +1,17 @@
+﻿namespace Main.Interfaces
+{
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Audio;
+    using Microsoft.Xna.Framework.Content;
+    using Microsoft.Xna.Framework.GamerServices;
+    using Microsoft.Xna.Framework.Graphics;
+    using Microsoft.Xna.Framework.Input;
+    using Microsoft.Xna.Framework.Media;
+
+    public interface IProjectile
+    {
+        Vector2 Position { get; set; }
+        Vector2 Velocity { get; set; }
+        bool IsVisible { get; set; }
+    }
+}
