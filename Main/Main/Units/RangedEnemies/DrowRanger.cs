75926f78aa38a43a29cb79fcbb28dff171d28d46
YoTsenkov <yotsenkov@gmail.com>
2014-02-24 20:00:49 +0200

    Added new ranged enemy and fixed the shooting for the ranged to properly find all Ranged enemies in the enemy list and makes them shoot

---
+++ Main/Main/Units/RangedEnemies/DrowRanger.cs
@@ -0,0 +1,12 @@
+﻿namespace Main
+{
+    class DrowRanger : Ranged
+    {
+         public DrowRanger(int positonX, int positionY, int rectangleWidth = 50, int rectangleHeight = 50)
+            : base(positonX, positionY, rectangleWidth, rectangleHeight)
+        {
+            this.Health = 2;
+            this.spriteName = "DrowRanger";
+        }
+    }
+}
