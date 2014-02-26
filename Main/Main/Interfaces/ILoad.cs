0ef2335cb9f94f01537db19456b298a97a3b5c5f
Martin Dzhonov <martin.dzhonov@gmail.com>
2014-02-26 17:23:01 +0200

    Interfaces

--- Main/Main/Interfaces/ILoad.cs
+++ Main/Main/Interfaces/ILoad.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Main
+namespace Main.Interfaces
 {
-    interface IMovable
+    interface Interface1
     {
     }
 }
